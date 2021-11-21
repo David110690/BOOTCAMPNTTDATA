2b14a0fbd0cf7b19b13374bb81e45f631190b368
David Travezaño <Davidu.0622@gmail.com>
2021-11-21 17:56:19 -0500

    create parent repository

---
+++ src/main/java/NTTDATA/demo/BootcampnttdataApplication.java
@@ -0,0 +1,13 @@
+package NTTDATA.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootcampnttdataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootcampnttdataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/NTTDATA/demo/BootcampnttdataApplicationTests.java
@@ -0,0 +1,13 @@
+package NTTDATA.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootcampnttdataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
